434209bf201b0fdcefaed26a5bc85777b56b1e33
JohnMnyika <jmnyika514@gmail.com>
2023-08-10 01:39:07 +0300

    added the backgound image and improve the home screen

--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+
+import backgroundImage from '../assets/background.jpg'; 
 
 export default function HomeScreen({ navigation }) {
   const handleGetStarted = () => {
@@ -7,45 +9,52 @@ export default function HomeScreen({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Mental Health App</Text>
-      <Text style={styles.subtitle}>Your Journey to Wellness</Text>
-      <TouchableOpacity style={styles.button} onPress={handleGetStarted}>
-        <Text style={styles.buttonText}>Get Started</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground source={backgroundImage} style={styles.background}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome to the Mental Health App</Text>
+        <Text style={styles.subtitle}>Embark on a Journey to Wellness</Text>
+        <TouchableOpacity style={styles.button} onPress={handleGetStarted}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover', 
+  },
   container: {
     flex: 1,
-    backgroundColor: '#F3F4F6',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 15,
     textAlign: 'center',
+    color: '#FFF', 
   },
   subtitle: {
-    fontSize: 16,
-    color: '#666',
+    fontSize: 18,
     marginBottom: 30,
     textAlign: 'center',
+    color: '#DDD', 
   },
   button: {
     backgroundColor: '#007BFF',
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 5,
+    paddingVertical: 14,
+    paddingHorizontal: 40,
+    borderRadius: 25,
   },
   buttonText: {
     color: 'white',
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: 'bold',
   },
 });
